6b269fe091d9a5b19e8eefdd606b9a696779b054
MateusLopes <mateus20.lopes02@gmail.com>
2022-10-27 23:02:32 -0300

    feat: adicionando animacao no card

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let isIgnite = true
+const cardAnimation = document.querySelector('.card-animaion')
 function changeCard(event) {
     const card = event.currentTarget
     const bg = isIgnite ? 'explorer' : 'ignite'
     isIgnite = !isIgnite
     card.style.backgroundImage = `url(./assets/card-${bg}.svg)`
+    cardAnimation.classList.toggle('card-animaion')
+    card.classList.toggle('rotate-card')
 }
